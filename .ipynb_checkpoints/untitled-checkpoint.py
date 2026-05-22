ccc12f24e0e78ad109c2546fb4d186ca194a1fb3
Lamis Sfaxi <lamissfaxi19@gmail.com>
2026-05-22 17:19:52 +0100

    remove large files and clean ml project

--- .ipynb_checkpoints/untitled-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('../data/creditcard.csv')
-print(df.shape)
-print(df['Class'].value_counts())
-df.head()
